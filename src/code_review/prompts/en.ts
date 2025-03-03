f911666f327f60ebcad702cd31ad3f0f1bf409fd
mys1024 <mys.1024@outlook.com>
2025-03-03 17:02:55 +0800

    feat: improve builtin prompts

--- src/code_review/prompts/en.ts
+++ src/code_review/prompts/en.ts
@@ -7,33 +7,35 @@ export function genEnBuiltinPrompt(replacements: PromptReplacements) {
 
 Your response should adhere to the following rules:
 
-1. Your response should follow the format of the response template.
-2. Your complete response **should not** be wrapped in code block delimiters (i.e., "\`\`\`").
-3. In the response template, sections, tables, etc., may only list one or two items as examples, but you should adjust the number of items according to the actual context.
-4. In the "File-wise Review" section, the "(filename)" should not include its path. However, when files with the same name exist, the "(filename)" should include the shortest path necessary to distinguish them.
-5. The review comments should indicate possible problems, shortcomings, and provide repair suggestions.
-6. The review comments should praise good changes.
+- Your response should follow the format of the response template.
+- The response template provided below will be wrapped in a code block, but your response **should not** be wrapped in code block symbols (i.e., "\`\`\`markdown" and "\`\`\`"). To emphasize, **do not use code blocks to wrap your response**.
+- In the response template, \`{{}}\` is the part you need to replace, for example, you should replace \`{{overall intent}}\` with the actual overall intent.
+- In the response template, sections, tables, etc., may only list one or two items as examples, but you should adjust the number of items according to the actual context.
+- The code changes provided below are usually represented in \`diff\` format, where the file paths may be prefixed with \`a/\` or \`b/\`. In the "File-wise Review" section, \`{{full file path}}\` **should not** include the prefixes \`a/\` or \`b/\`.
+- In the "File-wise Review" section, the "{{filename}}" should not include its path. However, when files with the same name exist, the "{{filename}}" should include the shortest path necessary to distinguish them.
+- The review comments should indicate possible problems, shortcomings, and provide repair suggestions.
+- The review comments should praise good changes.
 
 This is the response template you need to follow:
 
 \`\`\`markdown
-# Overall Intents
+# Overall Intent
 
-1. (overall intent 1)
+{{overall intent}}
 
 # Overall Changes
 
-1. (overall change 1)
+1. {{overall change 1}}
 
 # Overall Review Comments
 
-1. (overall review comment 1)
+1. {{overall review comment 1}}
 
 # File-wise Review
 
-| File                                 | Changes                        | Change Intents                               | Review Comments                                |
-| ------------------------------------ | ------------------------------ | -------------------------------------------- | ---------------------------------------------- |
-| [(filename 1)](<(full file path 1)>) | 1. (change 1)<br>2. (change 2) | 1. (change intent 1)<br>2. (change intent 2) | 1. (review comment 1)<br>2. (review comment 2) |
+| File                                   | Changes                            | Change Intents                                   | Review Comments                                     |
+| -------------------------------------- | ---------------------------------- | ------------------------------------------------ | --------------------------------------------------- |
+| [{{filename 1}}]({{full file path 1}}) | 1. {{change 1}}<br>2. {{change 2}} | 1. {{change intent 1}}<br>2. {{change intent 2}} | 1. {{review comment 1}}<br>2. ({{review comment 2}} |
 \`\`\`
 
 This is the code changes you need to review:
